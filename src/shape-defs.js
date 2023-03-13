4d4e2726dcabf17de03814404e5075569dc65ab8
johnamii <81453889+johnamii@users.noreply.github.com>
2023-03-13 13:17:26 -0700

    defined class inheritance, entities, boundaries

--- src/shape-defs.js
+++ src/shape-defs.js
@@ -4,26 +4,6 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Texture, Scene,
 } = tiny;
 
-export class Skybox extends defs.Cube{
-    constructor() {
-        super();
-        this.arrays.color = [];
-        for (let i = 0; i < 24; i++){
-            this.arrays.color.push(color(.5, .8, 1, .9));
-        }
-    }
-}
-
-export class Ground extends defs.Square {
-    constructor(){
-        super();
-
-        this.arrays.texture_coord = Vector.cast(
-            [0, 0], [4, 0], [0, 4], [4, 4]
-         )
-    }
-}
-
 export class Axis extends Shape {
     constructor(){
         super("position");
@@ -40,22 +20,50 @@ export class Axis extends Shape {
         }
     }
 }
-export class Building extends defs.Cube{
-    constructor(){
-        super();
 
+export class Skybox extends defs.Cube{
+    constructor() {
+        super();
         this.arrays.color = [];
         for (let i = 0; i < 24; i++){
-            this.arrays.color.push(color(.69, .42, .165, 1));
+            this.arrays.color.push(color(.5, .8, 1, .9));
         }
-
-        this.position = this.arrays.position;
     }
 }
 
-export class ModifiedCube extends Shape {
+class Boundary extends defs.Square {
+    constructor(){
+        super();
+
+        
+    }
+}
+
+export class Ground extends Boundary {
+    constructor(){
+        super();
+
+        this.arrays.texture_coord = Vector.cast(
+            [0, 0], [4, 0], [0, 4], [4, 4]
+        );
+    }
+}
+
+export class Wall extends Boundary {
+    constructor(){
+        super();
+
+        this.arrays.texture_coord = Vector.cast(
+            [0, 0], [4, 0], [0, 4], [4, 4]
+        );
+    }
+}
+
+export class BoundaryBox extends Shape {
     constructor(){
         super("position", "normal", "texture_coord");
+
+        this.boundary_transforms = []
 
         for (let i = 0; i < 3; i++)
                 for (let j = 0; j < 2; j++) {
@@ -64,20 +72,103 @@ export class ModifiedCube extends Shape {
                         .times(Mat4.translation(0, 0, 1));
                     // Calling this function of a Square (or any Shape) copies it into the specified
                     // Shape (this one) at the specified matrix offset (square_transform):
-                    Ground.insert_transformed_copy_into(this, [], square_transform);
+                    Wall.insert_transformed_copy_into(this, [], square_transform);
+                    this.boundary_transforms.push(square_transform);
                 }
     }
 }
 
-export class Starship extends defs.Cube {
-    constructor(){
+class Entity extends defs.Cube {
+    constructor(start_pos, speed_mult){
         super();
 
-        this.arrays.color = [];
-        for (let i = 0; i < 24; i++){
-            this.arrays.color.push(color(.9, .9, .9, 1));
+        this.speed_multiplier = speed_mult ?? 1;
+        this.meters_per_frame = 10;
+        this.thrust = vec3(0, 0, 0);
+        this.turn = 0;
+        this.dims = [1, 1, 1];
+        this.transform = Mat4.identity()
+            .times(Mat4.translation(start_pos[0], start_pos[1], start_pos[2]))
+            //.times(Mat4.scale(this.dims[0], this.dims[1], this.dims[2]))
+            ;
+    }
+
+    checkCollisions(entities){
+
+    }
+
+    onCollision(){
+
+    }
+
+    animate(){
+
+    }
+
+    doMovememnt(dt){
+        let speed = dt * this.meters_per_frame * this.speed_multiplier;
+        if (this.thrust[1] < 0) {
+            this.thrust[1] += 0.05
         }
+
+         // get y coordinate of center of starship, fall until hitting ground
+         let transformY = this.transform[1][3] - this.dims[1];
+         if (transformY > 0.1 ){
+             let fall = -0.2;
+             this.transform = this.transform.times(Mat4.translation(0, fall, 0));
+         }
+
+        this.transform.post_multiply(Mat4.rotation(.025 * this.turn, 0, 1, 0));
+        this.transform.post_multiply(Mat4.translation(...this.thrust.times(-speed)));
+    }
+}
+
+export class Starship extends Entity {
+    constructor(start_pos, speed_mult){
+        super(start_pos, speed_mult);
         
+        this.dims = [0.7, 0.5, 1];
+        this.transform = this.transform.times(Mat4.scale(this.dims[0], this.dims[1], this.dims[2]))
         //defs.Capped_Cylinder.insert_transformed_copy_into(this, [10, 10], wheel_transform);
     }
+
+    doMovement(dt){
+        
+    }
+}
+
+export class PowellCat extends Entity {
+    constructor(start_pos){
+        super(start_pos);
+
+
+    }
+
+
+}
+
+export class Target extends Entity {
+    constructor(start_pos){
+        super(start_pos);
+
+
+    }
+}
+
+export class Student extends Entity {
+    constructor(start_pos, speed_mult){
+        super(start_pos, speed_mult);
+
+
+    }
+
+
+}
+
+export class PowerUp extends Entity {
+    constructor(start_pos) {
+        super(start_pos);
+
+    }
+
 }
